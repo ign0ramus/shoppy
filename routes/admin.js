0589ea7645f2692de46060ea289ec58a6dc893f8
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-02-10 12:33:57 +0300

    adjust views

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,14 @@ const express = require('express');
 const {
 	handleGetAddProduct,
 	handlePostAddProduct,
-} = require('../controllers/products');
+	handleGetProducts,
+} = require('../controllers/admin');
 
 const router = express.Router();
 
 router.get('/add-product', handleGetAddProduct);
+router.get('/products', handleGetProducts);
+
 router.post('/add-product', handlePostAddProduct);
 
 module.exports = router;
