0b1ec5c8088e114e1cca26e6d639b19501dbb08f
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-02-17 17:46:52 +0300

    add helmet

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const MongoDbStore = require('connect-mongodb-session')(session);
 const csrfProtection = require('csurf')();
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
 
 const connectToMongoose = require('./src/database/db');
 const adminRoutes = require('./src/routes/admin');
@@ -54,6 +55,7 @@ app.use(
 );
 app.use(csrfProtection);
 app.use(flash());
+app.use(helmet());
 
 app.use((req, res, next) => {
 	res.locals.isAuthenticated = req.session.userId;
